c32f036e965e46d008a92fdabd894ff0a76923f7
Daniel Dupont <Daniel Dupont>
2016-04-12 14:04:36 +0200

    #2 add score orderBy desc

--- resources/views/game/leaderboard.blade.php
+++ resources/views/game/leaderboard.blade.php
@@ -3,12 +3,11 @@
 @section('content')
  <ul>
  <?php
-  $editeurs = DB::table('users')->select('name')->get();
+  $editeurs = DB::table('users')->select('name', 'score')->orderBy('score', 'desc')->get();
    foreach ($editeurs as $editeur) {
-   echo '<li>' . $editeur->name . '</li>';
+   echo '<li>' . $editeur->name . '</li>' .
+        '<li>' . $editeur->score . '</li><br/>';
   }
-?>
-  <li>hello world</li>
-  <li>yolo</li>
+ ?>
  </ul>
 @endsection
