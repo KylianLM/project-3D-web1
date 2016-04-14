4a78703170046d05880cac03bf75ade6699ee43f
Kylian LE METTE <kylian.le.mette@gmail.com>
2016-04-14 15:24:16 +0200

    Add leaderboard

--- resources/views/game/leaderboard.blade.php
+++ resources/views/game/leaderboard.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.app')
+{{--Inclusion des link CSS--}}
+@section('linkrel')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/leaderboard.css')}}"/>
+@endsection
 
-@section('content')
- <ul>
- <?php
-  $editeurs = DB::table('users')->select('name', 'score')->orderBy('score', 'desc')->get();
-   foreach ($editeurs as $editeur) {
-   echo '<li>' . $editeur->name . '</li>' .
-        '<li>' . $editeur->score . '</li><br/>';
-  }
- ?>
-
-
- </ul>
-
+@section('leader')
+    <ul>
+        <h1>Classement</h1>
+        <?php
+        $editeurs = DB::table('users')->select('name', 'score')->orderBy('score', 'desc')->paginate(3);
+        foreach ($editeurs as $editeur) { ?>
+        <li><?= $editeur->name ?> <span><?= $editeur->score ?> points</span></li>
+        <li><?= $editeur->name ?> <span><?= $editeur->score ?> points</span></li>
+        <?php
+        }
+        ?>
+    </ul>
 @endsection
 
