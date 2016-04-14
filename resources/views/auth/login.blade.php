bc927c78b7236aac230d41be8259f5275400b631
Daniel Dupont <Daniel Dupont>
2016-04-14 16:12:55 +0200

    login fini

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,40 +6,48 @@
 <div class="container">
     <h1>Connexion</h1>
     <p>Devenez un pilote à bord de votre avion</p>
-    <div class="panel-body">
-        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-            {!! csrf_field() !!}
+    <div id="centrage_bloc">
+        <div class="panel-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                {!! csrf_field() !!}
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label class="col-md-4 control-label">E-Mail Address</label>
-                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-                    @if ($errors->has('email'))
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label class="col-md-4 control-label">E-Mail Address</label>
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label class="col-md-4 control-label">Password</label>
+                    <input type="password" class="form-control" name="password">
+
+                    @if ($errors->has('password'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
+                            <strong>{{ $errors->first('password') }}</strong>
                         </span>
                     @endif
+                </div>
+
+                <label>
+                    <input type="checkbox" name="remember" class="rememberMe"> Remember Me
+                </label>
+
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-btn fa-sign-in"></i>Login
+                </button>
+
+                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+            </form>
+        </div>
+        <div id="redirectionInscription">
+            <div id="centrage_contenu">
+                <p>Vous n'avez pas de compte</p>
+                <button>Créer un compte</button>
             </div>
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label class="col-md-4 control-label">Password</label>
-                <input type="password" class="form-control" name="password">
-
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-
-            <label>
-                <input type="checkbox" name="remember" class="rememberMe"> Remember Me
-            </label>
-
-            <button type="submit" class="btn btn-primary">
-                <i class="fa fa-btn fa-sign-in"></i>Login
-            </button>
-
-            <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-        </form>
+        </div>
     </div>
 </div>
 @endsection
